35bbf6c1752bfe53e01469333187d63eef5da704
Sören <wegener92@googlemail.com>
2012-06-16 17:44:56 +0200

    Use the "full conversation" url to mark as read.

--- background.js
+++ background.js
@@ -1,13 +1,6 @@
 chrome.webRequest.onBeforeRequest.addListener(function(details) {
-  if (details.url.indexOf("explicit=true") != -1) {
-    return {
-      url: 'https://www.facebook.com/ajax/mercury/change_read_status.php?__a=1'
-    }
-  }
-  else {
-    return {
-      cancel: true
-    }
+  return {
+    cancel: true
   }
 }, { urls: ["*://*.facebook.com/*change_read_status*"] }, ["blocking"]
 )
--- read_button.js
+++ read_button.js
@@ -1,29 +1,12 @@
-function get_thread_id() {
-  var chunk = $('script:contains("thread_id")').text()
-  var pos = chunk.indexOf('(')
-  var data = JSON.parse(chunk.slice(pos + 1, chunk.length - 1)) // somewhere in there are some "thread_id"s
-}
-
 function add_read_buttons() {
   $('.fbMercuryChatTab').each(function(i) {
     if ($(this).find('.mark_as_read').length == 0) {
-      // a.titlebarText
-      // .fbChatConvItem a.profileLink
       $(this).find('.typingIndicator').after('<a><div class="mark_as_read inputbutton">Mark as read</div></a>')
+      var thread_url = $(this).find('a.itemAnchor[href*="facebook.com/messages/"]').attr('href')
       $(this).find('.mark_as_read').click(function() {
-        var dtsg = $('input[name="fb_dtsg"]').attr('value')
-        var re = /user=(\d*)/
-        var user = re.exec($('a.topborder').attr('ajaxify'))[1]
-        var thread_id = get_thread_id()
         $.ajax({
-          type: 'POST',
-          url: 'https://www.facebook.com/ajax/mercury/change_read_status.php?__a=1&explicit=true',
-          data: {
-            'ids[id.xxxxxxxxxxxxxxx]': true, // x are digits
-            'fb_dtsg': dtsg,
-            '__user': user
-            //"phstamp": "xxxxxxxxxxxxxxxxxxxxxxx" // seems irrelevant
-          }
+          type: 'GET',
+          url: thread_url
         })
       })
     }
