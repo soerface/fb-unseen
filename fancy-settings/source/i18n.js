3708d2152fae95018aef91782663898b48078003
Sören <wegener92@googlemail.com>
2012-09-16 21:31:51 +0200

    Removed git stuff out of fancy-settings.
    Instead tracking every file in this main repo.

---
+++ fancy-settings/source/i18n.js
@@ -0,0 +1,42 @@
+this.i18n = {
+  'blocks': {
+    'en': 'Blocks',
+    'de': 'Sperren'
+  },
+  'block_chat_seen': {
+    'en': 'Block the ”seen” feature of the chat',
+    'de': '„Gelesen“ Funktion des Chats blockieren'
+  },
+  'block_group_seen': {
+    'en': 'Block the ”seen” feature in groups',
+    'de': '„Gelesen“ Funktion in Gruppen blockieren'
+  },
+  'block_typing_indicator': {
+    'en': 'Block the typing indicator of the chat',
+    'de': '„Schreibt etwas“ Funktion blockieren'
+  },
+  'general': {
+    'en': 'General',
+    'de': 'Allgemein'
+  },
+  'interface': {
+    'en': 'Interface',
+    'de': 'Oberfläche'
+  },
+  'nothing-found': {
+    'en': 'No matches were found.',
+    'de': 'Keine Übereinstimmungen gefunden.'
+  },
+  'search': {
+    'en': 'Search',
+    'de': 'Suche'
+  },
+  'show_mark_as_read': {
+    'en': 'Show the ”Mark as read” Button (reload of facebook page required)',
+    'de': '„Als gelesen markieren“ Knopf anzeigen (Neuladen der Facebook Seite erforderlich)'
+  },
+  'settings': {
+    'en': 'Settings',
+    'de': 'Optionen'
+  }
+};
