7f0e41a45fba57580c5665b8dab95974925b15ae
Sören <wegener92@googlemail.com>
2012-09-16 20:52:41 +0200

    Read settings in blocking functions.

--- background.js
+++ background.js
@@ -8,10 +8,15 @@ var settings = new Store('settings', {
 
 chrome.webRequest.onBeforeRequest.addListener(function(details) {
   return {
-    cancel: true
+    cancel: settings.get('block_chat_seen')
   }
-}, { urls: ['*://*.facebook.com/*change_read_status*'] }, ['blocking']
-)
+}, { urls: ['*://*.facebook.com/*change_read_status*'] }, ['blocking'])
+
+chrome.webRequest.onBeforeRequest.addListener(function(details) {
+  return {
+    cancel: settings.get('block_typing_indicator')
+  }
+}, { urls: ['*://*.facebook.com/*typ.php*'] }, ['blocking'])
 
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
   if (request.action == 'getSettings') {
