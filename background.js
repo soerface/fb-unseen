14a287c9ccec27dd9de22823e2565d4be37217d0
Sören <wegener92@googlemail.com>
2012-09-16 14:28:41 +0200

    Removed background.html page, it only contained scripts.
    Set default settings.

--- background.js
+++ background.js
@@ -1,5 +1,9 @@
 var settings = new Store('settings', {
-    'show_mark_as_read': true
+    'show_mark_as_read': true,
+    'block_chat_seen': true,
+    'block_typing_indicator': false
+    // TODO
+    // 'block_group_seen': false
 });
 
 chrome.webRequest.onBeforeRequest.addListener(function(details) {
