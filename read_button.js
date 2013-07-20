866fffb9bc297bf338310af5235bae914b28f22c
Soeren Wegener <wegener92@gmail.com>
2013-07-21 01:40:35 +0200

    Fixed mark as read button \o/

--- read_button.js
+++ read_button.js
@@ -1,34 +1,64 @@
+function matching_brackets(data) {
+  var open_brackets = 0;
+  for(var i=0; i<data.length; i++) {
+    if (data[i] == '[') {
+      open_brackets++;
+    } else if (data[i] == ']') {
+      open_brackets--;
+    }
+    if (open_brackets == 0) {
+      return data.slice(0, i+1);
+    }
+  }
+}
+
 function add_read_buttons() {
-  $('.fbMercuryChatTab').each(function(i) {
+  $('.fbNubFlyout').each(function(i) {
     if ($(this).find('.mark_as_read').length == 0) {
-      $(this).find('.typingIndicator').after('<div class="mark_as_read inputbutton">Mark as read</div>')
-      //var thread_url = $(this).find('a.itemAnchor[href*="facebook.com/messages/"]').attr('href')
+      $(this).find('._510g').after('<div class="mark_as_read inputbutton">Mark as read</div>');
+      var full_conversation_url = $(this).find('a.itemAnchor[href*="facebook.com/messages/"]').attr('href')
+      var threads;
+      $.ajax({
+        type: 'GET',
+        url: full_conversation_url
+      }).done(function ( data ) {
+        var exp = /^\<script\>bigPipe\.onPageletArrive\((.*threads":(.*))\)\<\/script\>$/m;
+        exp.exec(data);
+        threads = JSON.parse(matching_brackets(RegExp.$2));
+      });
+      var exp = /.*facebook.com\/messages\/(.*)$/;
+      exp.exec(full_conversation_url);
+      var partner_id = 'fbid:' + RegExp.$1;
+      var thread_url = 'https://m.facebook.com/messages/read/?tid=';
       $(this).find('.mark_as_read').click(function() {
-        alert("Sorry, this button does not work anymore since a recent update of the facebook messenger. It will be disabled now, when I found a way how to make it work again, the button will reappear. However, blocking the `Seen: XX:XX` is unaffected and is still successfully blocked, only this button to manually mark it as read is broken. If you are interested in the technical details, visit http://swege.github.io/fb-unseen/ (will be opened in new tab)")
-        chrome.extension.sendRequest({action: 'setDisableButton'})
-        $(this).remove()
-        /*$(this).addClass('inactive')
+        var conversation_id = '';
+        for (var i=0; i<threads.length; i++) {
+          if ($.inArray(partner_id, threads[i].participants) != -1) {
+            conversation_id = threads[i].thread_id;
+            break;
+          }
+        }
+        $(this).addClass('inactive');
         $.ajax({
           type: 'GET',
-          url: thread_url
-        })*/
+          url: thread_url + conversation_id
+        });
+        var button = $(this)
+        setTimeout(function() {
+          button.removeClass('inactive');
+        }, 600);
       })
-    }
-    else {
-      $(this).find('.mark_as_read').removeClass('inactive')
     }
   })
 }
 
 $(document).ready(function() {
   chrome.extension.sendRequest({action: 'getSettings'}, function(settings) {
-    chrome.extension.sendRequest({action: 'getDisableButton'}, function(forceDisableButton) {
-      if (false && settings.show_mark_as_read && localStorage['force_disable_button'] != 'true' && forceDisableButton != 'true') {
-        $('#fbDockChatTabs').bind('DOMNodeInserted', function() {
-          add_read_buttons()
-        })
+    if (settings.show_mark_as_read) {
+      $('#ChatTabsPagelet').bind('DOMNodeInserted', function() {
         add_read_buttons()
-      }
-    })
+      })
+      add_read_buttons()
+    }
   })
 })
