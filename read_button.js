b672f18d3f4df841ccc2d47d1fcd986ffe843147
Soeren Wegener <wegener92@gmail.com>
2013-07-21 02:18:07 +0200

    Improved performance of mark as read button.

--- read_button.js
+++ read_button.js
@@ -13,10 +13,11 @@ function matching_brackets(data) {
 }
 
 function add_read_buttons() {
-  $('.fbNubFlyout').each(function(i) {
+  $('.vBot').each(function(i) {
     if ($(this).find('.mark_as_read').length == 0) {
       $(this).find('._510g').after('<div class="mark_as_read inputbutton">Mark as read</div>');
-      var full_conversation_url = $(this).find('a.itemAnchor[href*="facebook.com/messages/"]').attr('href')
+      var fbnub = $(this).parents('.fbNubFlyoutInner');
+      var full_conversation_url = fbnub.find('a.itemAnchor[href*="facebook.com/messages/"]').attr('href')
       var threads;
       $.ajax({
         type: 'GET',
@@ -29,24 +30,24 @@ function add_read_buttons() {
       var exp = /.*facebook.com\/messages\/(.*)$/;
       exp.exec(full_conversation_url);
       var partner_id = 'fbid:' + RegExp.$1;
-      var thread_url = 'https://m.facebook.com/messages/read/?tid=';
+      var read_url = 'https://m.facebook.com/messages/read/?tid=';
       $(this).find('.mark_as_read').click(function() {
-        var conversation_id = '';
-        for (var i=0; i<threads.length; i++) {
-          if ($.inArray(partner_id, threads[i].participants) != -1) {
-            conversation_id = threads[i].thread_id;
-            break;
-          }
-        }
-        $(this).addClass('inactive');
-        $.ajax({
-          type: 'GET',
-          url: thread_url + conversation_id
-        });
         var button = $(this)
+        button.addClass('inactive');
         setTimeout(function() {
+          var conversation_id = '';
+          for (var i=0; i<threads.length; i++) {
+            if ($.inArray(partner_id, threads[i].participants) != -1) {
+              conversation_id = threads[i].thread_id;
+              break;
+            }
+          }
+          $.ajax({
+            type: 'GET',
+            url: read_url// + conversation_id
+          });
           button.removeClass('inactive');
-        }, 600);
+        }, 2000);
       })
     }
   })
